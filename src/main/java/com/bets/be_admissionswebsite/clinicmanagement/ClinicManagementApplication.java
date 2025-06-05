7e1b2953ba1174b8cb21694811cf4169ad7cc666
DANG CONG NHAT <dangcongnhat2004qb@gmail.com>
2025-06-05 08:38:50 +0700

    first commit

---
+++ src/main/java/com/bets/be_admissionswebsite/clinicmanagement/ClinicManagementApplication.java
@@ -0,0 +1,13 @@
+package com.bets.be_admissionswebsite.clinicmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bets/be_admissionswebsite/clinicmanagement/ClinicManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bets.be_admissionswebsite.clinicmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
